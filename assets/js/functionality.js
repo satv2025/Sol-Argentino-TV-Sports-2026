298d9575acb8c2ca69200294aaf6093dcc0b2740
satv2025 <traplegionrecords2020@gmail.com>
2026-03-31 01:58:25 -0300

    Update functionality.js

--- assets/js/functionality.js
+++ assets/js/functionality.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const sb = supabase.createClient(
     "https://api.solargentinotv.com.ar",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNwemd4dmtlZHNkampoenp5eXNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk1MzQwOTAsImV4cCI6MjA4NTExMDA5MH0.RgFghlZVV4Ww27rfh96nTiafDwRu9jtC3S6Y6aFdIxE"
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNwemd4dmtlZHNkampoenp5eXNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk1MzQwOTAsImV4cCI6MjA4NTExMDA5MH0.RgFghlZVV4Ww27rfh96nTiafDwRu9jtC3S6Y6aFdIxE"
 );
 
 const $ = id => document.getElementById(id);
